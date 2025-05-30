3e861f6fe0f139085a7297fc8377875d45c6f0a9
Kumar Harsh Raj <harshcodes.sshhr@gmail.com>
2025-05-30 14:17:16 +0530

    Add user creation endpoint and database table setup

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask
+
 app= Flask(__name__)
+
 @app.route('/')
 def test_api():
     return 'Hello, World!'
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route('/createUser',methods =['POST'])
+def create_user():
+
+
+
+    if __name__=='__main__':
+        app.run(debug=True)
